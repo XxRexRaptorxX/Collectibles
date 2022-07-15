f7ad87913ddb2adb9d015cf7bd9c0549eb9edc61
XxRexRaptorxX <Raptor-Simon@web.de>
2022-07-15 17:49:46 +0200

    config structure rework

--- src/main/java/xxrexraptorxx/collectibles/utils/Config.java
+++ src/main/java/xxrexraptorxx/collectibles/utils/Config.java
@@ -15,6 +15,8 @@ import java.util.List;
 public class Config {
 
     public static final String CATEGORY_GENERAL = "general";
+    public static final String CATEGORY_COLLECTIBLES = "collectibles";
+    public static final String CATEGORY_LOOT_BAG = "loot_bag";
 
     public static ForgeConfigSpec SERVER_CONFIG;
     public static ForgeConfigSpec CLIENT_CONFIG;
@@ -29,6 +31,7 @@ public class Config {
     public static ForgeConfigSpec.IntValue COLLECTIBLES_XP;
     public static ForgeConfigSpec.IntValue FRAGMENT_COLLECTIBLE_RARITY;
     public static ForgeConfigSpec.IntValue COIN_COLLECTIBLE_RARITY;
+    public static ForgeConfigSpec.BooleanValue PATREON_REWARDS;
 
     public static void init() {
         initServer();
@@ -54,7 +57,17 @@ public class Config {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
         builder.comment("General").push(CATEGORY_GENERAL);
+        PATREON_REWARDS = builder.comment("Enables ingame rewards on first spawn for Patreons").define("patreon_rewards", true);
+        builder.pop();
+
+        builder.comment("Collectibles").push(CATEGORY_COLLECTIBLES);
         COLLECTIBLES_XP = builder.comment("How much xp a player should get when he finds a collectible").defineInRange("collectible_xp", 30, 0, 1000);
+        COIN_COLLECTIBLE_RARITY = builder.comment("How rarely should coin collectibles drop. higher = rarer [1:X]").defineInRange("coin_collectibles_rarity", 1000, 1, 100000);
+        FRAGMENT_COLLECTIBLE_RARITY = builder.comment("How rarely should fragment collectibles drop. higher = rarer [1:X]").defineInRange("fragment_collectibles_rarity", 1000, 1, 100000);
+        builder.pop();
+
+
+        builder.comment("Loot Bag").push(CATEGORY_LOOT_BAG);
         EPIC_LOOT_BAG_XP = builder.comment("How much xp a player should get when he uses a loot bag").defineInRange("epic_loot_bag_xp", 100, 0, 1000);
         EPIC_LOOT_BAG_REWARDS = builder.comment("A list with all epic loot bag rewards [amount*modid:item]").define("epic_loot_bag_rewards", new ArrayList<>(Arrays.asList(
                 "1*" + ForgeRegistries.ITEMS.getKey(Items.NETHERITE_INGOT).toString(),
@@ -65,7 +78,7 @@ public class Config {
                 "1*" + ForgeRegistries.ITEMS.getKey(Items.ENCHANTED_GOLDEN_APPLE).toString(),
                 "5*" + ForgeRegistries.ITEMS.getKey(Items.DRAGON_BREATH).toString(),
                 "10*" + ForgeRegistries.ITEMS.getKey(Items.DIAMOND).toString()
-                )));
+        )));
         EPIC_LOOT_BAG_ITEM_AMOUNT = builder.comment("How many item entries the epic loot bag gives the player").defineInRange("epic_loot_bag_item_amount", 1, 1, 10);
         LOOT_BAG_REWARDS = builder.comment("A list with all loot bag rewards [amount*modid:item]").define("loot_bag_rewards", new ArrayList<>(Arrays.asList(
                 "3*" + ForgeRegistries.ITEMS.getKey(Items.EMERALD).toString(),
@@ -75,9 +88,8 @@ public class Config {
         )));
         LOOT_BAG_XP = builder.comment("How much xp a player should get when he uses a loot bag").defineInRange("loot_bag_xp", 50, 0, 1000);
         LOOT_BAG_ITEM_AMOUNT = builder.comment("How many item entries the loot bag gives the player").defineInRange("loot_bag_item_amount", 1, 1, 10);
-        COIN_COLLECTIBLE_RARITY = builder.comment("How rarely should coin collectibles drop. higher = rarer [1:X]").defineInRange("coin_collectibles_rarity", 1000, 1, 100000);
-        FRAGMENT_COLLECTIBLE_RARITY = builder.comment("How rarely should fragment collectibles drop. higher = rarer [1:X]").defineInRange("fragment_collectibles_rarity", 1000, 1, 100000);
         builder.pop();
+
 
         SERVER_CONFIG = builder.build();
     }
